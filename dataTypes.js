e4f3eac3dc0227f4b45a3b30263c55d0787f7c6f
[ayush tiwari] <[ayush0511tiwari@gmail.com]>
2024-08-14 02:28:46 +0530

    upload

---
+++ dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// Javascript engine is inside the browser
+
+// alert(3+3) // we are using nodejs, not browser
+
+
+// In javascript and react we generally ignore (;)
+
+console.log(3+3)   // code readibility should be high
+
+console.log("Ayush")
+
+// eg of data types in javascript
+
+let name = "Ayush"  // string data type
+let age = "22" // number data type
+let isLoggedIn = false // boolean data type
+
+
+
+
+
+// number => 2 to power 53
+// bigint => for very big numbers
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined 
---
+++ variables.js
@@ -0,0 +1,8 @@
+const accountId = 144445
+let accountEmail = "ayush@google.com"
+var accountPassword = "12345"
+accountcity = "Jaipur"
+
+// accountId = 2 // not allowed
+
+console.log(accountId);
