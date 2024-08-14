4cb555f0094aa13aa81be2743e8a0ef3fcef078c
[ayush tiwari] <[ayush0511tiwari@gmail.com]>
2024-08-15 02:14:39 +0530

    upload

---
+++ conversionOperation.js
@@ -0,0 +1,39 @@
+let score = 33
+
+// we can determine its type by two methods
+
+console.log(typeof score); // direct using typeof
+console.log(typeof(score)); // as method
+
+// now if we see the result , it will show
+// number
+// number
+
+// and if we put 33 in "33" then it will give the output 
+// string
+// string
+
+// now we can do one thing
+// important:-
+// let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+// NOTE- In Javascript, there is no strict check, that's why people use typescript
+
+
+let isLoggedIn = "Ayush"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Ayush" => true
+
+
